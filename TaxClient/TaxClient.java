ca839b7940916a28bc751ad93fc03cc60d3dca5a
Brendan <brendan.rogerz@gmail.com>
2017-08-20 18:01:58 +1000

    Initial commit

---
+++ TaxClient/TaxClient.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Brendan on 20/08/2017.
+ */
+public class TaxClient {
+
+    public static void main(String[] args) {
+        TaxClient client = new TaxClient();
+        client.start();
+    }
+
+    public void start(){
+
+    }
+}
---
+++ TaxServer/TaxServer.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Brendan on 20/08/2017.
+ */
+public class TaxServer {
+
+    public static void main(String[] args) {
+        TaxServer server = new TaxServer();
+        server.start();
+    }
+
+    public void start(){
+
+    }
+
+}
