24dae215191900664d2cd2532831a99543f2ea8e
Brendan <brendan.rogerz@gmail.com>
2017-08-20 20:22:56 +1000

    started building tax server

--- TaxClient/TaxClient.java
+++ TaxClient/TaxClient.java
@@ -1,3 +1,7 @@
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 /**
  * Created by Brendan on 20/08/2017.
  */
@@ -9,6 +13,24 @@ public class TaxClient {
     }
 
     public void start(){
+        String hostName = "localhost";
+        int portNumber = 12345;
 
+        try (
+                Socket echoSocket = new Socket(hostName, portNumber);
+                PrintWriter out =
+                        new PrintWriter(echoSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+                BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
+        ) {
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+                System.out.println("echo: " + in.readLine());
+            }
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            System.exit(1);
+        }
     }
 }
--- TaxServer/TaxServer.java
+++ TaxServer/TaxServer.java
@@ -1,3 +1,7 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by Brendan on 20/08/2017.
  */
@@ -9,7 +13,37 @@ public class TaxServer {
     }
 
     public void start(){
+        int portNumber = 12345;
+        int[] storedValues = {0,0,0,0};
 
+        try (
+                ServerSocket serverSocket = new ServerSocket(portNumber);
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        ) {
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                switch (inputLine){
+                    case "TAX\n":
+                        out.println("TAX: OK\n");
+                        break;
+                    case "STORE\n":
+                        for (int i = 0; i < 4; i++) {
+                            storedValues[i] = Integer.parseInt(inputLine);
+                        }
+                        out.println("STORE: OK");
+                        break;
+                    case "QUERY\n":break;
+                    case "BYE\n":break;
+                    case "END\n":break;
+
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Exception caught when trying to listen on port "
+                    + portNumber + " or listening for a connection");
+            System.out.println(e.getMessage());
+        }
     }
-
 }
